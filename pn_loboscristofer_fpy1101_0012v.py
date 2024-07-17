6d22128047f994555d134d10d81a3ee448934a9c
Cristoferlobos <cr.loboso@duocuc.cl>
2024-07-17 19:53:02 -0400

    trabajadores

--- pn_loboscristofer_fpy1101_0012v.py
+++ pn_loboscristofer_fpy1101_0012v.py
@@ -1,3 +1,8 @@
 import random
 import csv 
 from math import prod 
+
+trabajadores = ['juan perez','maria garcia','carlos lopez', 'ana martines',
+                'pedro rodrigez ', 'laura hernandez', 'miguel sanchez', 'isabel gomez', 'franciscodias',
+                'elena fernandez']
+
